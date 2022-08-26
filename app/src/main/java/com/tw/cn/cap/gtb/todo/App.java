915cbf195a404c4c19785e347df873b1203598d5
zxh <415109435@qq.com>
2022-08-26 19:30:08 +0800

    refactor: extract readTasksLines method

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author zxh
+ */
 public class App {
 
     public static void main(String[] args) {
@@ -12,11 +15,16 @@ public class App {
     }
 
     public List<String> run() {
+        final List<String> result = new ArrayList<>();
+        result.add("# To be done");
+        final List<String> lines = readTasksLines();
+        result.addAll(lines);
+        return result;
+    }
+
+    private List<String> readTasksLines() {
         try {
-            final List<String> result = new ArrayList<>();
-            result.add("# To be done");
-            result.addAll(Files.readAllLines(Constant.TASKS_FILE_PATH));
-            return result;
+            return Files.readAllLines(Constant.TASKS_FILE_PATH);
         } catch (IOException e) {
             throw new TodoCannotReadFileException();
         }
