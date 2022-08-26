31e13241bbd78694ea26b226f3d468b13d86a616
zxh <415109435@qq.com>
2022-08-26 16:55:26 +0800

    chore: show flie content

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,11 +1,21 @@
 package com.tw.cn.cap.gtb.todo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+    public List<String> run() {
+        try {
+            return Files.readAllLines(Path.of("/Users/zxh/.todo/tasks"));
+        } catch (IOException e) {
+            throw new TodoCannotReadFileException();
+        }
     }
 }
--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -1,13 +1,17 @@
 package com.tw.cn.cap.gtb.todo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+
     @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    void should_list_existing_tasks() {
+        final List<String> result = new App().run();
+        Assertions.assertEquals(List.of("task 01","task 02"), result);
     }
 }
