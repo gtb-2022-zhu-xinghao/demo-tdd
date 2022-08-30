839a161d8df0552e82c8f0b325e1c4506adc785d
zxh <415109435@qq.com>
2022-08-30 21:08:34 +0800

    refactor: extract MyTaskRepository class in test

--- app/src/test/java/com/tw/cn/cap/gtb/todo/AddCommandTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AddCommandTest.java
@@ -1,5 +1,6 @@
 package com.tw.cn.cap.gtb.todo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,25 +8,41 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddCommandTest {
+
+    private MyTaskRepository taskRepository;
+
+    @BeforeEach
+    void setUp() {
+        taskRepository = new MyTaskRepository();
+    }
+
     @Test
     void should_multiple_words_as_name() {
-        final var taskRepository = new TaskRepository() {
-            private Task task;
-
-            @Override
-            List<String> createTask(Task task) {
-                this.task = task;
-                return List.of();
-            }
-
-            public String getTaskName() {
-                return this.task.getName();
-            }
-        };
         final AddCommand command = new AddCommand(taskRepository, "add", "fizz", "buzz");
         command.execute();
 
-        final var taskName = taskRepository.getTaskName();
-        assertEquals("fizz buzz", taskName);
+        assertEquals("fizz buzz", taskRepository.getTaskName());
+    }
+    @Test
+    void should_use_empty_name_when_no_args_provided() {
+
+        final AddCommand command = new AddCommand(taskRepository, "add");
+        command.execute();
+
+        assertEquals("", taskRepository.getTaskName());
+    }
+
+    private static class MyTaskRepository extends TaskRepository {
+        private Task task;
+
+        @Override
+        List<String> createTask(Task task) {
+            this.task = task;
+            return List.of();
+        }
+
+        public String getTaskName() {
+            return this.task.getName();
+        }
     }
 }
